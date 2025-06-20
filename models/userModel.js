95144ed672daffb8b3b4eb80becc6979fcbd97c2
Liz Alvarez Scudeller <157268952+liz-scudeller@users.noreply.github.com>
2025-06-20 03:29:24 +0000

    first commit

---
+++ models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase: true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Role',
+        required:true
+    }
+
+
+},{timestamps:true});
+
+module.exports = mongoose.Model('User', userSchema);
